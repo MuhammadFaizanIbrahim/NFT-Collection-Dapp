9d39dfb2d572c6c8281baaa81312e14bc50412fb
Faizan <mfaizanibrahim25@gmail.com>
2023-01-02 18:17:49 +0500

    Ok

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xa86652e6e151175eae0Aa6A432235732A497878a";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,6 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-};
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {
+    },
+    goerli: {
+      url: "https://boldest-wider-layer.ethereum-goerli.discover.quiknode.pro/0447a5232535ba5077476c817766ab32a2cbdde8",
+      accounts: ["28f5ea1b74dda607d1b080b28614266cc66176c7c35b9a09c3a0a6e75c284c41"],
+}
+  },
+  solidity: {
+    version: "0.8.15",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+ 
+}
